894f2a8cd6036a35a95e3ac545f84df9499e6980
rabKarim <163855688+RabKarim@users.noreply.github.com>
2024-04-07 21:10:58 +0100

    added functionality to the grid button

--- js-file.js
+++ js-file.js
@@ -3,8 +3,8 @@ const grid_button = document.createElement("button")
 grid_button.setAttribute("id", "grid_button")
 grid_button.textContent = "Grid number"
 
-let grid = document.createElement("div")
-grid.setAttribute("id", "grid")
+
+
 let num_of_pixels = 6
 
 
@@ -16,12 +16,14 @@ function range(from, to) {
         number_range.push(i)
     }
     return number_range
-    
+
 }
 
 
 
 function create_grid() {
+    let grid = document.createElement("div")
+    grid.setAttribute("id", "grid")
     let dimensions = Math.ceil(600 / num_of_pixels)
     let array = range(1,(num_of_pixels**2))
     for (let b = 0; b < array.length; b++) {
@@ -30,7 +32,7 @@ function create_grid() {
         pixel.style.width = dimensions + "px"
         pixel.style.height = dimensions + "px"
         pixel.style.backgroundColor = "black"
-        console.log(String(dimensions))
+        container.appendChild(grid)
         grid.appendChild(pixel)
         pixel.addEventListener("mouseover", function(e){
             pixel.style.backgroundColor = "white"
@@ -40,10 +42,18 @@ function create_grid() {
 
 
 }
+grid_button.addEventListener("click", function(e){
+    grid.remove()
+    let response = prompt("Enter a number between 2-100")
+    num_of_pixels = Number(response)
+    create_grid()
+    
 
+})
 
 
 container.appendChild(grid_button)
-container.appendChild(grid)
 create_grid()
+
+
 
